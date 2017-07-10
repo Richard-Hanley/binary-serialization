3d7c0b9da893f444771b00ced170917e697be274
Richard Hanley <rhanley@iit.edu>
2017-07-10 12:13:29 -0500

    Adding in a few spec records for codecs

--- src/binary_serialization/codec.clj
+++ src/binary_serialization/codec.clj
@@ -9,20 +9,13 @@
   (to-buffer* [this value buffer])
   (from-buffer* [this buffer]))
 
-(defn specify 
-  "Takes a codec, and a specification, and returns a new codec
-  that applies the spec before processing the data"
-  [codec spec])
-
-(defn constant 
-  "Takes a codec, and a specific value, and returns a new
-  codec that will only accept nil or the value, and conforms to the value"
-  [codec value])
-
-(defn default
-  "Takes a codec and a value.  Returns a new codec that accepts a value
-  or nil.  If the value is nil, then the default value is returned"
-  [codec value])
+(extend-protocol Codec
+  nil
+  (primitive* [_] true)
+  (conform* [_ _] nil)
+  (sizeof* [_ _] 0)
+  (to-buffer* [_ _ _] nil)
+  (from-buffer* [_ _ _] nil))
 
 (defn primitive? [codec] (primitive* codec))
 (defn composite? [codec] (not (primitive? codec)))
@@ -39,3 +32,22 @@
 (defn from-buffer!
   [codec buffer] (from-buffer* codec buffer))
 
+(defrecord CodecSpec [spec codec])
+
+(defn specify 
+  "Takes a codec, and a specification, and returns a new codec
+  that applies the spec before processing the data"
+  [codec spec])
+
+(defn constant 
+  "Takes a codec, and a specific value, and returns a new
+  codec that will only accept nil or the value, and conforms to the value"
+  [codec value])
+
+(defn default
+  "Takes a codec and a value.  Returns a new codec that accepts a value
+  or nil.  If the value is nil, then the default value is returned"
+  [codec value])
+
+
+(defrecord AssciatedField [field codec])
